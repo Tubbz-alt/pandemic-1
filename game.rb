ac8a6727da307c5300e006a5616529a1580fb416
cmdrtorefresh <cmdrtorefresh@gmail.com>
2016-02-11 12:05:15 -0800

    prompt players info such as name and roles

--- game.rb
+++ game.rb
@@ -4,28 +4,71 @@ require_relative "player"
 
 class Game
 
-  attr_accessor :number_players, :difficulty, :infection_deck, :infection_discard_pile
-  attr_reader :infection_rate_index, :players
+  attr_accessor :number_players, :infection_deck, :infection_discard_pile
+  attr_reader :infection_rate_index, :players, :available_roles_to_pick, :deal_player_card_number, :epidemic_cards_number
 
-  def initialize(number_players, difficulty)
-    @number_players = number_players
+  def initialize
+    @number_players = 0
+    prompt_number_of_players
+
+    @epidemic_cards_number = 0
+    determine_epidemic_cards_number
+
     @infection_rate_index = 0
     @board = Board.new()
     @infection_deck = setup_infection_deck #Index 0 = Bottom of deck
     @infection_discard_pile = []
+    @player_deck = setup_player_deck #Index 0 = Bottom of deck
     @players = []
+    @available_roles_to_pick = ROLES.keys.shuffle
 
-    # case difficulty
-    # when "introductory":
-    #   @difficulty = "4 Epidemic Cards"
-    # when "standard"
-    #   @difficulty = "5 Epidemic Cards"
-    # when "heroic"
-    #   @difficulty = "6 Epidemic Cards"
-    # else
-    #   raise "Acceptable inputs are only 'introductory', 'standard', or 'heroic'!"
-    # end
     create_players
+    prompt_player_info
+    determine_deal_player_card_number
+  end
+
+  def prompt_number_of_players
+    while @number_players < 2 or @number_players > 5 do
+      print "Enter number of players (2 to 4)! "
+      @number_players = gets.chomp.to_i
+    end
+      puts "There are "+@number_players.to_s+" players in this game"
+      puts
+  end
+
+
+  def determine_epidemic_cards_number
+    while @epidemic_cards_number == 0 do
+      print "Pick difficulty level, 'i' for introductory, 's' for standard, or 'h' for heroic! "
+      difficulty = gets.chomp.downcase
+
+      case difficulty
+      when "i"
+        @epidemic_cards_number = 4
+      when "s"
+        @epidemic_cards_number = 5
+      when "h"
+        @epidemic_cards_number = 6
+      else
+        puts "The only options are 'i', 's' or 'h'!"
+      end
+
+    end
+
+    puts "This game's difficulty level is "+difficulty.to_s
+    puts
+  end
+
+
+  def determine_deal_player_card_number
+    case @number_players
+    when 2
+      @deal_player_card_number = 4
+    when 3
+      @deal_player_card_number = 3
+    when 4
+      @deal_player_card_number = 2
+    end
   end
 
 
@@ -36,8 +79,48 @@ class Game
     end
   end
 
+  def prompt_player_info
+    @players.each_with_index do |player, idx|
+      prompt_player_name(idx)
+      puts
+      prompt_player_role(idx)
+      puts
+    end
+  end
 
-  def infection_rate
+  def prompt_player_name(idx)
+    print "Enter Name of Player "+(idx + 1).to_s+"! "
+    @players[idx].name = gets.chomp
+    puts
+  end
+
+  def prompt_player_role(idx)
+    puts "The available roles are "+@available_roles_to_pick.to_s
+    print @players[idx].name.to_s+", please choose a number between 1 to "+@available_roles_to_pick.size.to_s+"! "
+    chosen_role_index = gets.chomp.to_i
+    @available_roles_to_pick.shuffle!
+    @players[idx].role = @available_roles_to_pick[chosen_role_index-1]
+    @available_roles_to_pick.delete(@players[idx].role)
+    puts @players[idx].name.to_s+", your role is "+@players[idx].role.to_s
+    puts
+  end
+
+  def prompt_player_take_cards(from)
+
+  end
+
+
+  def players_info #API
+    @players.each_with_index do |player, idx|
+      puts "Player "+(idx+1).to_s+" = "+player.name.to_s+", role = "+player.role.to_s
+    end
+  end
+
+
+
+
+
+  def infection_rate #API
     INFECTION_RATE_BOARD[@infection_rate_index]
   end
 
@@ -51,6 +134,11 @@ class Game
 
   def setup_infection_deck
     @board.infection_cards.shuffle!
+  end
+
+  def setup_player_deck
+    @board.player_cards.shuffle!
+
   end
 
   def deal_card(from, number_of_cards = 1)
--- player.rb
+++ player.rb
@@ -3,13 +3,13 @@ require_relative 'game'
 
 class Player
 
-  attr_accessor :name, :role, :city, :cards
+  attr_accessor :name, :role, :location, :cards
 
   def initialize(name, role)
     @name = name
     @role = role
-    @cards
-    # @city = CityObject
+    @cards = []
+    # @location = CityObject
   end
 
   def move_pawn(player = self, destination)
