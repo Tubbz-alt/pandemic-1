ec25c9b338b2358ba7bd9036099778aa66375143
cmdrtorefresh <cmdrtorefresh@gmail.com>
2016-02-25 15:36:56 -0800

    List all saved files if any has been created

--- pandemic.rb
+++ pandemic.rb
@@ -1,5 +1,6 @@
 require_relative 'game'
 require 'yaml'
+require 'fileutils'
 
 class Pandemic
 
@@ -10,24 +11,45 @@ class Pandemic
   def setup
     print "Do you want to load a saved game? Type 'y' for confirmation! "
     if gets.chomp == "y"
-      print "Please enter a yaml filename (without .yml extension)! "
-      filename = gets.chomp
-      @g = load_game_file(filename)
-      puts "Game file #{filename}.yml has been loaded."
-      puts
-      @g.play
-    else
-      print "New file is created. The game will be autosaved. Please assign a new file name (without .yml extension)! "
-      filename = gets.chomp
-      puts "A pandemic game with filename #{filename}.yml has been created."
-      puts
-      @g = Game.new(filename)
+      if saved_games_folder?
+        puts "Folder exists"
+        list_files = all_yml_files_in_saved_games
+        if list_files.size > 0
+          puts "Available filenames = " + list_files.to_s
+          print "Please enter a yaml filename (without .yml extension)! "
+          filename = gets.chomp
+          @g = load_game_file(filename)
+          puts "Game file #{filename}.yml has been loaded."
+          puts
+          return @g.play
+        end
+      end
+      puts "No saved games so far!"
     end
+
+    print "New file will be created. The game will be autosaved. Please assign a new file name (without .yml extension)! "
+    filename = gets.chomp
+    puts
+    @g = Game.new(filename)
+
   end
 
   def load_game_file(filename)
     file = File.read("saved_games/#{filename}.yml")
     @g = YAML::load(file)
+  end
+
+  def saved_games_folder?
+    File.exist?('saved_games')
+  end
+
+  def all_yml_files_in_saved_games
+    files = Dir.glob("saved_games/*.yml")
+    file_names = []
+    files.each do |file|
+      file_names << file[12..-5]
+    end
+    file_names
   end
 
 end
